c10711dd206748534fac098a29bf062a25bdac8c
Jon3Lee <59454890+Jon3Lee@users.noreply.github.com>
2020-11-16 03:47:42 -0800

    Update GameBot.py

--- GameBot.py
+++ GameBot.py
@@ -6,5 +6,11 @@ client = commands.Bot(command_prefix = 'g.')
 @client.event
 async def on_ready():
 	print('Game Bot is ready.')
+	
+@client.event
+async def on_message(message):
+
+	if message.content.startswith("What's up bot"):
+		await message.channel.send("Ur trash kid get good at life")
 
 client.run('TOKEN OMITTED')
