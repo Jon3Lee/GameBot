50525c4e1ea19705964e33d0c27728d15ee33e5b
Jon3Lee <59454890+Jon3Lee@users.noreply.github.com>
2020-11-16 03:39:43 -0800

    Update GameBot.py

--- GameBot.py
+++ GameBot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!jk')
+client = commands.Bot(command_prefix = 'g.')
 
 @client.event
 async def on_ready():
