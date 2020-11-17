7ca54edf88b02e9dacda7e402378e2c228c73aba
Jon3Lee <59454890+Jon3Lee@users.noreply.github.com>
2020-11-16 22:36:27 -0800

    Update GameBot.py

--- GameBot.py
+++ GameBot.py
@@ -7,14 +7,19 @@ client = commands.Bot(command_prefix = 'g.')
 @client.event
 async def on_ready():
 	print('Game Bot is ready.')
-	
+
 @client.command()
 async def ping(ctx):
-	await ctx.send(f'Ping! Response time is {round(client.latency * 1000)}ms')
-	
-@client.command(aliases = ['fortune' , 'question'])
-async def YesNo(ctx, *, question):
-	responses = ['Yes.', 'No.']
+	await ctx.send(f'Pong! Response time is {round(client.latency * 1000)}ms')
+
+@client.command(aliases = ['fortune' , 'for'])
+async def yesno(ctx, *, question):
+	responses = ['Yes.', 'No.', 'Only time will tell.', 'What kind of question even is that?', 'lmao no', 'Maybe? Ask again', 'Only god can answer this one.', 
+	'Look at yourself in the mirror before you ask that question.', 'Most definitely.', 'Hell no']
 	await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command()
+async def roll(ctx, num=100):
+	await ctx.send(f"{ctx.author.mention}'s roll is: {random.randrange(0,num)}")
 	
 client.run('TOKEN OMITTED')
