645a99960913a9f3bb44fcceaad26439d1dc2570
Jon3Lee <3jonlee3@gmail.com>
2020-11-17 17:46:19 -0800

    update

--- bot/cogs/autorole.py
+++ bot/cogs/autorole.py
@@ -6,18 +6,18 @@ class autorole(commands.Cog):
 	def __init__ (self, client):
 		self.client = client
 
-	
+	@commands.Cog.listener()
 	async def on_member_join(self, member):
 		#await member.send(f"Welcome {member.name} to Game Server! <TEST>") #Sends a DM to member on join.
-		await client.get_channel(768660907433000994).send(f'Welcome {member.name} to Game!')
-		await member.add_roles(226897097012674571)
+		await self.get_channel(768660907433000994).send(f'Welcome {member.name} to Game!')
+		await self.add_roles(member, 226897097012674571)
 
 
-	
+	@commands.Cog.listener()
 	async def on_member_remove(self, member):
-		await client.get_channel(768660907433000994).send(f'Goodbye! {member.name} has left Game.')
+		await self.get_channel(768660907433000994).send(f'Goodbye! {member.name} has left Game.')
 
-	
+	@commands.Cog.listener()
 	async def on_message(self, message):
 		if message.content.startswith("What's up bot"):
 	 		await message.channel.send("Hey! Thanks for creating me!")
