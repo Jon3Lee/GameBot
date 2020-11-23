e502ab5d7202792181249641eebe22a62979df22
Jon3Lee <3jonlee3@gmail.com>
2020-11-22 23:04:28 -0800

    update

--- bot/cogs/reactionrole.py
+++ bot/cogs/reactionrole.py
@@ -7,20 +7,36 @@ class reactionrole(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        roleChannelid = ''
+        roleChannelid = '780317227421401108'
 
+        #If not in roles channel, do nothing.
         if reaction.message.channel.id != roleChannelid:
             return
-        # if str(reaction.emoji) == '':
-        #     await client.add_roles(user, role)
+        
+        #Add roles per reaction addition
+        if str(reaction.emoji) == '👍':
+            role = discord.utils.get(user.guild.roles, name = 'wow')
+            await self.client.add_roles(user, role)
+        elif str(reaction.emoji) == '<:valorant:780320609946566667>':
+            role = discord.utils.get(user.guild.roles, name = 'valorant')
+            await self.client.add_roles(user, role)
+
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-        roleChannelid = ''
+        roleChannelid = '780317227421401108'
+
+        #If not in roles channel, do nothing.
         if reaction.message.channel.id != roleChannelid:
             return
-        # if str(reaction.emoji) == '':
-        #     await client.remove_roles(user, role)
+
+        #Remove roles per reaction removal
+        if str(reaction.emoji) == '<:wow:780323199732416564>':
+            role = discord.utils.get(user.guild.roles, name = 'wow')
+            await self.client.remove_roles(user, role)
+        elif str(reaction.emoji) == '<:valorant:780320609946566667>':
+            role = discord.utils.get(user.guild.roles, name = 'valorant')
+            await self.client.remove_roles(user, role)
 
 def setup(client):
     client.add_cog(reactionrole(client))
